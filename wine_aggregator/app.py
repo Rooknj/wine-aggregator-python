068b94dcc21d76659c7b6e74240d58eeec4ac21e
Nick Rook <rooknj@gmail.com>
2023-11-11 13:32:05 -0800

    chore: add logging config

--- wine_aggregator/app.py
+++ wine_aggregator/app.py
@@ -1,5 +1,13 @@
+import logging
+import logging.config
+
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print("Hello, World!")
+    logger.info("Hello World")  # will not print anything
+    logger.warning("Watch out!")  # will print a message to the console
 
 
 if __name__ == "__main__":
