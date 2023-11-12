9f04d877b3be96aab1c5edcf0707563c2b6af3df
Nick Rook <rooknj@gmail.com>
2023-11-12 15:42:39 -0800

    chore: switch to ruff (#3)

--- tests/acceptance/features/steps/example.py
+++ tests/acceptance/features/steps/example.py
@@ -1,31 +1,31 @@
-from behave import *
+from behave import given, when, then
 
 
 @given("we have behave installed")
-def step_impl(context):
+def example_given_step_impl(context):
     pass
 
 
 @when("we implement a test")
-def step_impl(context):
+def example_when_step_impl(context):
     assert True is not False
 
 
 @then("behave will test it for us!")
-def step_impl(context):
+def example_then_step_impl(context):
     assert context.failed is False
 
 
 @given("I am interested in Purlieu")
-def step_impl(context):
+def interested_step_impl(context):
     pass
 
 
 @when("I get a new email from K&L Wine Merchants")
-def step_impl(context):
+def email_step_impl(context):
     pass
 
 
 @then("I should see 'Purlieu \"Le Pich\" Napa Valley Sauvignon Blanc (Elsewhere $30)'")
-def step_impl(context):
+def should_step_impl(context):
     pass
