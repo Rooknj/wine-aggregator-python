9c3f1c0b6b31bfc9eba9ed6ebe141db1d51d059c
Nick Rook <rooknj@gmail.com>
2024-02-11 11:22:28 -0800

    chore: fix lint

--- tests/acceptance/features/environment.py
+++ tests/acceptance/features/environment.py
@@ -2,7 +2,7 @@ from behave.model import Scenario  # type: ignore
 from behave.runner import Context  # type: ignore
 
 
-def before_scenario(context: Context, scenario: Scenario):
+def before_scenario(context: Context, scenario: Scenario) -> None:
     if "wip" in scenario.effective_tags:
         scenario.skip("Work in Progress")
     return
--- tests/acceptance/features/steps/example.py
+++ tests/acceptance/features/steps/example.py
@@ -1,31 +1,32 @@
 from behave import given, when, then  # type: ignore
+from behave.runner import Context  # type: ignore
 
 
-@given("we have behave installed")
-def example_given_step_impl(context):
+@given("we have behave installed")  # type: ignore
+def example_given_step_impl(context: Context) -> None:
     pass
 
 
-@when("we implement a test")
-def example_when_step_impl(context):
+@when("we implement a test")  # type: ignore
+def example_when_step_impl(context: Context) -> None:
     assert True is not False
 
 
-@then("behave will test it for us!")
-def example_then_step_impl(context):
+@then("behave will test it for us!")  # type: ignore
+def example_then_step_impl(context: Context) -> None:
     assert context.failed is False
 
 
-@given("I am interested in Purlieu")
-def interested_step_impl(context):
+@given("I am interested in Purlieu")  # type: ignore
+def interested_step_impl(context: Context) -> None:
     pass
 
 
-@when("I get a new email from K&L Wine Merchants")
-def email_step_impl(context):
+@when("I get a new email from K&L Wine Merchants")  # type: ignore
+def email_step_impl(context: Context) -> None:
     pass
 
 
-@then("I should see 'Purlieu \"Le Pich\" Napa Valley Sauvignon Blanc (Elsewhere $30)'")
-def should_step_impl(context):
+@then("I should see 'Purlieu \"Le Pich\" Napa Valley Sauvignon Blanc (Elsewhere $30)'")  # type: ignore
+def should_step_impl(context: Context) -> None:
     pass
